0243df1f20b4e3e5310546ddb36487b9cc83cf4e
zpro <marin223@naver.com>
2022-10-25 15:49:59 +0900

    show video list with title

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+youtube API key : AIzaSyAhoCez0b2I99DwAfxGzvfdO1gsMTGhY7w
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-    <h1>Hell12o</h1>
-  );
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		//javscdript fetch(youtube api)
+		const requestOptions = {
+			method: 'GET',
+			redirect: 'follow',
+		};
+
+		fetch(
+			'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAhoCez0b2I99DwAfxGzvfdO1gsMTGhY7w',
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				setVideos(result.items);
+			})
+			.catch((error) => console.log('error', error));
+	}, []); //두번째 변수가 없으면 매번, []빈 배열이 있으면 한 번만, 배열의 값(변수명)이 있으면 변수값이 변경될 때마다 callback
+
+	return <VideoList videos={videos} />;
 }
 
 export default App;
